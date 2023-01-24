11a4a2843f8fead7719b3d31cc960c8686e6c9a6
Aanis <aanishassan@hotmail.com>
2023-01-24 14:53:49 +0000

    more comments

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,14 +5,14 @@ require('dotenv').config()
 const requireAuth = (req,res,next) => {
     const token = req.cookies.b21 //b21 name of cookie
 
-    //
+    
  if(token) {
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedToken) => {  // 
-        if(err) {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedToken) => {  // method on jwt package
+        if(err) {           //2nd arguement is secret // 3rd is function takes in decoded token
             console.log(err.message)
-            res.redirect('/login')}   
+            res.redirect('/login')}   // error back to login screen
         else {
-            next()}
+            next()}   // verified
         })}
         
         else {
